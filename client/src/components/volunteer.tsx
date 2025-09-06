578b9046b71dda9a7b77c6c3f3d4f059f5dba29c
Hanzla-Nawaz <hanzlanawaz@outlook.com>
2025-09-07 03:39:07 +0500

    Add Stanford SVG image to volunteer section

--- client/src/components/volunteer.tsx
+++ client/src/components/volunteer.tsx
@@ -9,7 +9,7 @@ const volunteerWork = [
     title: "Section Leader Mentor - Stanford University",
     role: "Mentor",
     description: "Code in Place is a free, human-centered, intro-to-coding course from Stanford University aiming to offer the highest quality free coding course to many students from around the world. Section leader mentors meet multiple times a week to provide students with one-on-one help, and also provide covers for sections. This section leader mentor has successfully supported students through the first half of Stanford's flagship intro to Python course, CS106A. Section leader mentors gain interpersonal skills, practice engaging pedagogy, and demonstrate their mastery of basic Python principles.",
-    image: "https://github.com/Hanzla-Nawaz/ProfessionalPortfolio/blob/main/client/public/images/stanford.svg",
+    image: "/images/stanford.svg",
     credentialUrl: "https://digitalcredential.stanford.edu/check/D06E68630B9A74AC34268A5CD909C7DE855E5202777D399588E12EBF741530C7RjNrbWpnaHNNdTBvWFpqNjNPK3dPWVg5SVJuZXVPMTU5L2MxckhtRzlGZmlWZFhx"
   },
   {
